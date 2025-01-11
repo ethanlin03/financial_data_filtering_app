87d1aae1af7d1d4cd7bea89b2a97156621e09d48
Ethan Lin <midgetlin2003@gmail.com>
2025-01-11 14:20:11 -0800

    Final changes to app before styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [currFilter, setCurrFilter] = useState("null");
   const [currInputs, setCurrInputs] = useState({});
+  const [sortConfig, setSortConfig] = useState({ key: "date", direction: "asc" });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [filters, setFilters] = useState({
     dateRange: { start: 2020, end: 2025 },
     revenueRange: { min: 0, max: Infinity },
@@ -23,9 +26,6 @@ const App = () => {
     { key: "operatingIncome", label: "Operating Income", sortable: false },
   ]);
 
-  const [sortConfig, setSortConfig] = useState({ key: "date", direction: "asc" });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -37,6 +37,56 @@ const App = () => {
   const handleFilterSubmit = (formData) => {
     setCurrInputs(formData);
     console.log("Received form data in App:", formData);
+  };
+
+  const filterDate = (startDate, endDate) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      dateRange: { start: startDate, end: endDate },
+      revenueRange: { min: 0, max: Infinity },
+      netIncomeRange: { min: 0, max: Infinity },
+    }));
+  }
+
+  const filterRevenue = (minRevenue, maxRevenue) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      revenueRange: { min: minRevenue, max: maxRevenue },
+      netIncomeRange: { min: 0, max: Infinity },
+      dateRange: { start: 2020, end: 2025 },
+    }));
+  }
+
+  const filterNetIncome = (minIncome, maxIncome) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      netIncomeRange: { min: minIncome, max: maxIncome },
+      revenueRange: { min: 0, max: Infinity },
+      dateRange: { start: 2020, end: 2025 },
+    }));
+  }
+
+  const resetFilters = () => {
+    setCurrFilter("null");
+    setCurrInputs({});
+    setFilters({
+      dateRange: { start: 2020, end: 2025 },
+      revenueRange: { min: 0, max: Infinity },
+      netIncomeRange: { min: 0, max: Infinity },
+    });
+  };
+
+  const sortedData = [...filteredData].sort((a, b) => {
+    if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.direction === "asc" ? -1 : 1;
+    if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.direction === "asc" ? 1 : -1;
+    return 0;
+  });
+
+  const handleSort = (key) => {
+    setSortConfig((prev) => ({
+      key,
+      direction: prev.key === key && prev.direction === "asc" ? "desc" : "asc",
+    }));
   };
 
   useEffect(() => {
@@ -56,37 +106,14 @@ const App = () => {
 
   useEffect(() => {
     if (currFilter !== "null") {
-      if (currInputs.startingYear && currInputs.endingYear) {
-        setFilters((prevFilters) => ({
-          ...prevFilters,
-          dateRange: {
-            start: currInputs.startingYear,
-            end: currInputs.endingYear,
-          },
-          revenueRange: { min: 0, max: Infinity },
-          netIncomeRange: { min: 0, max: Infinity },
-        }));
-      } else if (currInputs.netIncomeMin && currInputs.netIncomeMax) {
-        setFilters((prevFilters) => ({
-          ...prevFilters,
-          netIncomeRange: {
-            min: currInputs.netIncomeMin,
-            max: currInputs.netIncomeMax,
-          },
-          revenueRange: { min: 0, max: Infinity },
-          dateRange: { start: 2020, end: 2025 },
-        }));
-      } else if (currInputs.revenueMin && currInputs.revenueMax) {
-        setFilters((prevFilters) => ({
-          ...prevFilters,
-          revenueRange: {
-            min: currInputs.revenueMin,
-            max: currInputs.revenueMax,
-          },
-          netIncomeRange: { min: 0, max: Infinity },
-          dateRange: { start: 2020, end: 2025 },
-        }));
-      }
+      if (currInputs.startingYear && currInputs.endingYear)
+        filterDate(currInputs.startingYear, currInputs.endingYear);
+
+      else if (currInputs.netIncomeMin && currInputs.netIncomeMax)
+        filterNetIncome(currInputs.netIncomeMin, currInputs.netIncomeMax);
+
+      else if (currInputs.revenueMin && currInputs.revenueMax)
+        filterRevenue(currInputs.revenueMin, currInputs.revenueMax);
     }
   }, [currFilter, currInputs]);
   
@@ -108,94 +135,52 @@ const App = () => {
     );
   }, [filters, data]);  
 
-  const sortedData = [...filteredData].sort((a, b) => {
-    if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.direction === "asc" ? -1 : 1;
-    if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.direction === "asc" ? 1 : -1;
-    return 0;
-  });
-
-  const handleSort = (key) => {
-    setSortConfig((prev) => ({
-      key,
-      direction: prev.key === key && prev.direction === "asc" ? "desc" : "asc",
-    }));
-  };
-
-  const filterDate = (startDate, endDate) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      dateRange: { start: startDate, end: endDate },
-    }));
-  }
-
-  const filterRevenue = (minRevenue, maxRevenue) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      revenueRange: { min: minRevenue, max: maxRevenue },
-    }));
-  }
-
-  const filterNetIncome = (minIncome, maxIncome) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      netIncomeRange: { min: minIncome, max: maxIncome },
-    }));
-  }
-
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-xl font-bold mt-10 mb-20">Apple Financial Data</h1>
-      <div className="flex flex-row">
-
-        <table className="table-auto border-collapse border border-gray-300 m-10">
-          <thead>
-          <tr className="bg-gray-200">
-            {columnHeaders.map((header) => (
-              <th
-                key={header.key}
-                className="p-4 text-left border border-gray-300"
-                onClick={() => header.sortable && handleSort(header.key)}
-              >
-                <button
-                  className={`arrow-button ${header.sortable ? "" : "cursor-default"}`}
-                  disabled={!header.sortable}
-                >
-                  {header.label}
-                  {(header.key === "date" || header.key === "revenue" || header.key === "netIncome") && header.sortable && (
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 20V10m0 10-3-3m3 3 3-3m5-13v10m0-10 3 3m-3-3-3 3"/>
-                    </svg>                
-                  )}
-                </button>
-              </th>
-            ))}
-          </tr>
+      <div className="flex items-start space-x-6">
+        <table className="table-auto border-collapse border border-gray-300 h-1/2 overflow-x-auto">
+          <thead className="bg-gray-200 sticky top-0 z-10">
+            <tr>
+              {columnHeaders.map((header) => (
+                <th key={header.key} className="p-4 text-left border border-gray-300" onClick={() => header.sortable && handleSort(header.key)}>
+                  <button className={`arrow-button ${header.sortable ? "" : "cursor-default"}`} disabled={!header.sortable}>
+                    <span className={`${sortConfig.key === header.key ? "text-[#57b9ff] font-bold" : "text-black"}`}>
+                      {header.label}
+                    </span>
+                    {(header.key === "date" || header.key === "revenue" || header.key === "netIncome") && header.sortable && (
+                      <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                        <path stroke={sortConfig.key === header.key && sortConfig.direction === "asc" ? "#57b9ff" : "black"} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 20V10m0 10-3-3m3 3 3-3" />
+                        <path stroke={sortConfig.key === header.key && sortConfig.direction === "desc" ? "#57b9ff" : "black"} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 3v10m0-10 3 3m-3-3-3 3" />
+                      </svg>
+                    )}
+                  </button>
+                </th>
+              ))}
+            </tr>
           </thead>
-          <tbody>
+          <tbody className="overflow-y-auto">
             {sortedData.map((row) => (
               <tr key={row.date} className="border border-gray-300">
-              <td className="p-4 text-left">{row.date}</td>
-              <td className="p-4 text-left">{row.revenue.toLocaleString()}</td>
-              <td className="p-4 text-left">{row.netIncome.toLocaleString()}</td>
-              <td className="p-4 text-left">{row.grossProfit.toLocaleString()}</td>
-              <td className="p-4 text-left">{row.eps}</td>
-              <td className="p-4 text-left">{row.operatingIncome.toLocaleString()}</td>
-            </tr>
+                <td className="p-4 text-left">{row.date}</td>
+                <td className="p-4 text-left">{row.revenue.toLocaleString()}</td>
+                <td className="p-4 text-left">{row.netIncome.toLocaleString()}</td>
+                <td className="p-4 text-left">{row.grossProfit.toLocaleString()}</td>
+                <td className="p-4 text-left">{row.eps}</td>
+                <td className="p-4 text-left">{row.operatingIncome.toLocaleString()}</td>
+              </tr>
             ))}
           </tbody>
         </table>
-        <button
-          onClick={openModal}
-          className="mb-72"
-        >
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M1 5h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 1 0 0-2H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2Zm18 4h-1.424a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2h10.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Zm0 6H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 0 0 0 2h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Z"/>
-        </svg>
-      </button>
 
-      <Modal isOpen={isModalOpen} onClose={closeModal} updateFilter={setCurrFilter} onApply={handleFilterSubmit}/>
-      
+        <button onClick={openModal} className="ml-6 mt-6">
+          <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M1 5h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 1 0 0-2H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2Zm18 4h-1.424a3.228 3.228 0 0 0-6.152 0H1a1 1 0 1 0 0 2h10.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Zm0 6H8.576a3.228 3.228 0 0 0-6.152 0H1a1 1 0 0 0 0 2h1.424a3.228 3.228 0 0 0 6.152 0H19a1 1 0 0 0 0-2Z" />
+          </svg>
+        </button>
+
       </div>
+      <Modal isOpen={isModalOpen} onClose={closeModal} updateFilter={setCurrFilter} onApply={handleFilterSubmit} resetFilters={resetFilters} />
     </div>
   );
 };
--- src/modal.jsx
+++ src/modal.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
+const Modal = ({ isOpen, onClose, updateFilter, onApply, resetFilters }) => {
   const [currentFilter, setCurrentFilter] = useState("date");
   const [formData, setFormData] = useState({});
+  const [formattedValues, setFormattedValues] = useState({});
 
   useEffect(() => {
     if (isOpen) {
       setFormData({});
+      setFormattedValues({});
       setCurrentFilter("date");
     }
   }, [isOpen]);
@@ -20,18 +21,36 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
     }
   };
 
+  const formatNumber = (value) => {
+    if (!value) return "";
+    return new Intl.NumberFormat("en-US").format(value);
+  };
+
+  const handleInputChange = (field, value) => {
+    const rawValue = value.replace(/,/g, "");
+    if (!isNaN(rawValue)) {
+      setFormData((prev) => ({
+        ...prev,
+        [field]: rawValue,
+      }));
+      setFormattedValues((prev) => ({
+        ...prev,
+        [field]: formatNumber(rawValue),
+      }));
+    }
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    resetFilters();
+    onClose();
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     updateFilter(currentFilter);
     onApply(formData);
-    onClose();    
-  };
-
-  const handleInputChange = (field, value) => {
-    setFormData((prev) => ({
-      ...prev,
-      [field]: value,
-    }));
+    onClose();
   };
 
   return (
@@ -40,13 +59,14 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
       onClick={handleClickOutside}
     >
       <div className="bg-white p-6 rounded-lg w-[80]">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-xl text-gray-700 hover:text-gray-900"
-        >
-          &times;
-        </button>
-        <h2 className="text-2xl font-semibold mb-4">Filter</h2>
+        <div className="flex flex-row justify-between">
+          <h2 className="text-2xl font-semibold mb-4">Filters</h2>
+          <button onClick={onClose} className="mb-8">
+            <svg className="w-6 h-6 text-gray-900 hover:text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+            </svg>
+          </button>
+        </div>
         <div className="flex flex-row justify-between space-x-2">
           <button
             className={`px-4 py-2 rounded border-2 ${
@@ -60,16 +80,6 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
           </button>
           <button
             className={`px-4 py-2 rounded border-2 ${
-              currentFilter === "netIncome"
-                ? "bg-gray-200 text-gray-500"
-                : "bg-transparent text-gray-500 hover:bg-gray-200"
-            }`}
-            onClick={() => setCurrentFilter("netIncome")}
-          >
-            Filter by Net Income
-          </button>
-          <button
-            className={`px-4 py-2 rounded border-2 ${
               currentFilter === "revenue"
                 ? "bg-gray-200 text-gray-500"
                 : "bg-transparent text-gray-500 hover:bg-gray-200"
@@ -78,7 +88,18 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
           >
             Filter by Revenue
           </button>
+          <button
+            className={`px-4 py-2 rounded border-2 ${
+              currentFilter === "netIncome"
+                ? "bg-gray-200 text-gray-500"
+                : "bg-transparent text-gray-500 hover:bg-gray-200"
+            }`}
+            onClick={() => setCurrentFilter("netIncome")}
+          >
+            Filter by Net Income
+          </button>
         </div>
+
         <form className="mt-4 p-4 border rounded" onSubmit={handleSubmit}>
           {currentFilter === "date" && (
             <div>
@@ -86,10 +107,7 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
                 Select Starting Year:
               </label>
               <input
-                type="number"
-                id="startingYear"
-                placeholder="Enter year (e.g., 2023)"
-                className="border px-4 py-2 rounded w-full mb-4"
+                type="number" id="startingYear" placeholder="Enter year (e.g., 2023)" className="border px-4 py-2 rounded w-full mb-4"
                 onChange={(e) =>
                   handleInputChange("startingYear", e.target.value)
                 }
@@ -98,10 +116,7 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
                 Select Ending Year:
               </label>
               <input
-                type="number"
-                id="endingYear"
-                placeholder="Enter year (e.g., 2023)"
-                className="border px-4 py-2 rounded w-full"
+                type="number" id="endingYear" placeholder="Enter year (e.g., 2023)" className="border px-4 py-2 rounded w-full"
                 onChange={(e) => handleInputChange("endingYear", e.target.value)}
               />
             </div>
@@ -113,22 +128,12 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
                 Enter Net Income Range (USD):
               </label>
               <input
-                type="number"
-                id="netIncomeMin"
-                placeholder="Minimum Net Income"
-                className="border px-4 py-2 rounded w-full mb-2"
-                onChange={(e) =>
-                  handleInputChange("netIncomeMin", e.target.value)
-                }
+                type="text" id="netIncomeMin" placeholder="Minimum Net Income" className="border px-4 py-2 rounded w-full mb-2" value={formattedValues.netIncomeMin || ""}
+                onChange={(e) => handleInputChange("netIncomeMin", e.target.value)}
               />
               <input
-                type="number"
-                id="netIncomeMax"
-                placeholder="Maximum Net Income"
-                className="border px-4 py-2 rounded w-full"
-                onChange={(e) =>
-                  handleInputChange("netIncomeMax", e.target.value)
-                }
+                type="text" id="netIncomeMax" placeholder="Maximum Net Income" className="border px-4 py-2 rounded w-full" value={formattedValues.netIncomeMax || ""}
+                onChange={(e) => handleInputChange("netIncomeMax", e.target.value)}
               />
             </div>
           )}
@@ -139,38 +144,26 @@ const Modal = ({ isOpen, onClose, updateFilter, onApply }) => {
                 Enter Revenue Range (USD):
               </label>
               <input
-                type="number"
-                id="revenueMin"
-                placeholder="Minimum Revenue"
-                className="border px-4 py-2 rounded w-full mb-2"
-                onChange={(e) =>
-                  handleInputChange("revenueMin", e.target.value)
-                }
+                type="text" id="revenueMin" placeholder="Minimum Revenue" className="border px-4 py-2 rounded w-full mb-2" value={formattedValues.revenueMin || ""}
+                onChange={(e) => handleInputChange("revenueMin", e.target.value)}
               />
               <input
-                type="number"
-                id="revenueMax"
-                placeholder="Maximum Revenue"
-                className="border px-4 py-2 rounded w-full"
-                onChange={(e) =>
-                  handleInputChange("revenueMax", e.target.value)
-                }
+                type="text" id="revenueMax" placeholder="Maximum Revenue" className="border px-4 py-2 rounded w-full" value={formattedValues.revenueMax || ""}
+                onChange={(e) => handleInputChange("revenueMax", e.target.value)}
               />
             </div>
           )}
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
-          >
-            Apply Filter
-          </button>
+
+          <div className="flex justify-between mt-4">
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+              Apply Filter
+            </button>
+            <button type="button" className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
+          
         </form>
-        <button
-          onClick={onClose}
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
-        >
-          Close
-        </button>
       </div>
     </div>
   );
